dc97d8451f2daa3626f37ea6077145f84b2fd2aa
nirajthanki6 <nirajthanki6@gmail.com>
2023-05-07 12:58:00 -0700

    Adventure Game

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,38 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road, it has come to an end and you can go left or right. which way you would like to go? ").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ").lower()
+
+    if answer == "swim":
+        print("You swim across and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game. ")
+    else:
+        print("Not a valid option. You lose. ")
+
+elif answer == "right":
+    answer = input(
+        "You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back) ? ").lower()
+    if answer == "back":
+        print("You go back to the main road Now you can decide to drive home.")
+    elif answer == "cross":
+        answer = input(
+            "You cross the bridge and meet a stranger. Do you talk to them (yes/no) ? ").lower()
+
+        if answer == 'yes':
+            print("Congratulations! you WIN :) ")
+        elif answer == 'no':
+            print("You missed the oppotunity and LOSE :( ")
+        else:
+            print("Not a valid option. You lose.")
+    else:
+        print("Not a valid option. You lose. ")
+else:
+    print("Not a valid option. You lose. ")
+
+print("Thank you for choosing Adventure game!")
